8ece9d05c1fa3d2e657ccc708f55432661dd1eb1
강바다 <kbd1120@naver.com>
2024-07-31 18:12:58 -0700

    :sparkles: [feat] 초기 설정 및 메인화면 구현

--- static/landing.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var landingbutton = document.querySelector('.landing_startbtn');
-    landingbutton.addEventListener("click", function() {
-        window.location.href = 'login.html';
-    });
-});
